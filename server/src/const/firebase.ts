dbe2d8c15a8196c6c49134412e4c2be01b0a4d40
BojanJankov <150026975+BojanJankov@users.noreply.github.com>
2024-10-05 19:18:50 +0200

    added api for app

---
+++ server/src/const/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZZJoRHLXxsoOyRTciKqxG9MZHnetHAwM",
+
+  authDomain: "thought-exchange-b66bf.firebaseapp.com",
+
+  projectId: "thought-exchange-b66bf",
+
+  storageBucket: "thought-exchange-b66bf.appspot.com",
+
+  messagingSenderId: "294754531668",
+
+  appId: "1:294754531668:web:53b790d5f206be2cae30c1",
+
+  measurementId: "G-7V4D8Z20DY",
+};
+
+initializeApp(firebaseConfig);
